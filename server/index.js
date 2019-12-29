d3e4b22f21fbdb4df905da41b45ffb254e8ed73d
Hayreddin Tüzel <hayreddin.tuzel@osf-commerce.com>
2019-12-29 15:46:56 +0300

    Created Server App - Installed core libraries

---
+++ server/index.js
@@ -0,0 +1,20 @@
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet')
+const config = require('config');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet())
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+});
