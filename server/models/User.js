64472ddb5cebb66df1b6467b5b8404508fba6347
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2020-01-05 16:59:45 +0300

    express unique validator

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 const bcrypt = require("bcryptjs"),
 
     saltRounds = 5,
@@ -45,5 +46,7 @@ UserSchema.methods.isCorrectPassword = function (password, callback) {
         }
     });
 };
+
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", UserSchema);
